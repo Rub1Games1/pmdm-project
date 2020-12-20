a1b8d65d9c9016147d10f0cc178ad5ac319d8021
Rub1Games1 <48184832+Rub1Games1@users.noreply.github.com>
2020-12-20 00:46:24 +0000

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,13 @@ $('.fa-angle-down').click(function() {
     var idToScroll = $(this).attr('value')
     document.getElementById(idToScroll).scrollIntoView({ behavior: "smooth" });
 })
+
+$('.card').hover(function() {
+    if($(this)[0].childNodes[3] != undefined) {
+        if($(this)[0].childNodes[3].tagName == "VIDEO") {
+            $(this)[0].childNodes[1].classList.toggle('display-none')
+            $(this)[0].childNodes[3].classList.toggle('display-none')
+            $(this)[0].childNodes[3].play()
+        }
+    }
+})
