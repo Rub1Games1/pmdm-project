1c3ba871b1a9fe868e30241707d5beae513a0607
Rub1Games1 <48184832+Rub1Games1@users.noreply.github.com>
2021-01-04 12:54:18 +0000

    Add files via upload

--- src/js/template.js
+++ src/js/template.js
@@ -5,33 +5,18 @@ var IndexNumber = parseInt(Index.slice(5,6))
 if (isNaN(IndexNumber) == true)
   IndexNumber = 0
 
-$(document).on("mousemove", function(e){
-    if (e.pageX < innerWidth * 0.075 && e.pageY > innerHeight * 0.075)
-        $('#leftMenu').show()
-    else
-        $('#leftMenu').hide()
-
-    if (e.pageX > innerWidth * 0.925 && e.pageY > innerHeight * 0.075)
-        $('#rightMenu').show()
-    else
-        $('#rightMenu').hide()
-});
-
-$(document).on("click", function(e){
-    if (e.pageX < innerWidth * 0.075 && e.pageY > innerHeight * 0.075) {
-        if (IndexNumber == 0)  
-          location.href = "index4.html"
-        else {
-          if(IndexNumber == 1)
-            location.href = "index.html"
-          else location.href = "index" + (IndexNumber - 1) + ".html"
-        }
-    }
-
-    if (e.pageX > innerWidth * 0.925 && e.pageY > innerHeight * 0.075) {
-      if(IndexNumber == 4)
-          location.href = "index.html"
-      else
-          location.href = "index" + (IndexNumber + 1) + ".html"
+$('#leftMenu').on("click", function(e){
+    if (IndexNumber == 0)  
+      location.href = "index4.html"
+    else {
+      if(IndexNumber == 1)
+        location.href = "index.html"
+      else location.href = "index" + (IndexNumber - 1) + ".html"
     }
 });
+$('#rightMenu').on("click", function(e){
+    if(IndexNumber == 4)
+        location.href = "index.html"
+    else
+        location.href = "index" + (IndexNumber + 1) + ".html"
+})
